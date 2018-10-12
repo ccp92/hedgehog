081e57553e7b4683de4f8e5907d761e2b21641d7
Christopher Parsons <mail@christopherparsons.me>
2018-10-12 14:13:24 +0100

    Set up folder structure for components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
 
 function loadStories() {
-  require('../src/stories');
+  const requireStories = require.context(
+    "../src/Components",
+    true,
+    /stories\.js$/
+  );
+  const children = requireStories.keys();
+
+  children.forEach(requireStories);
 }
 
 configure(loadStories, module);
