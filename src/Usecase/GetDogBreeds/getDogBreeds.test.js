97b4d645554a8d6e48170c887f311cde92d05bd0
Christopher Parsons <mail@christopherparsons.me>
2018-12-21 14:39:19 +0000

    Add second example to getDogBreeds.test

--- src/Usecase/GetDogBreeds/getDogBreeds.test.js
+++ src/Usecase/GetDogBreeds/getDogBreeds.test.js
@@ -3,20 +3,41 @@ import GetDogBreeds from "../GetDogBreeds";
 import nock from "nock";
 
 describe("GetDogBreeds", () => {
-  let data;
-  beforeEach(() => {
-    data = {
-      status: "success",
-      message: { affenpischer: [], african: [], airedale: [] }
-    };
-    nock("https://dog.ceo")
-      .get("/api/breeds/list/all")
-      .reply(200, data);
+  describe("Example 1", () => {
+    let data;
+    beforeEach(() => {
+      data = {
+        status: "success",
+        message: { affenpischer: [], african: [], airedale: [] }
+      };
+      nock("https://dog.ceo")
+        .get("/api/breeds/list/all")
+        .reply(200, data);
+    });
+
+    it("Should respond with a list of dogs", async () => {
+      const getDogBreeds = new GetDogBreeds();
+      let response = await getDogBreeds.execute();
+      expect(response).toEqual(data);
+    });
   });
 
-  it("Should respond with a list of dogs", async () => {
-    const getDogBreeds = new GetDogBreeds();
-    let response = await getDogBreeds.execute();
-    expect(response).toEqual(data);
+  describe("Example 2", () => {
+    let data;
+    beforeEach(() => {
+      data = {
+        status: "success",
+        message: { poodle: [], jack_russell: [], cockapoo: [], shitzu: [] }
+      };
+      nock("https://dog.ceo")
+        .get("/api/breeds/list/all")
+        .reply(200, data);
+    });
+
+    it("Should respond with a list of dogs", async () => {
+      const getDogBreeds = new GetDogBreeds();
+      let response = await getDogBreeds.execute();
+      expect(response).toEqual(data);
+    });
   });
 });
