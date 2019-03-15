bb893267cb162b6a235d2915fb629eb9facdbf90
Christopher Parsons <mail@christopherparsons.me>
2019-03-15 13:46:08 +0000

    Add nock to app.test.js to resolve nock error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import nock from "nock";
 
 it("renders without crashing", () => {
+  nock("https://dog.ceo")
+        .get(`/api/breeds/list/all`)
+        .reply(200, {});
+
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
