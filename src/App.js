15ef67bec3a600acb8d1ab05046afd63576b1f56
Christopher Parsons <mail@christopherparsons.me>
2019-04-27 02:21:22 +0100

    Update main app to be function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 
 import GetDogBreeds from "./Usecase/GetDogBreeds";
@@ -16,32 +16,31 @@ import "bootstrap/dist/css/bootstrap-theme.css";
 const getDogBreeds = new GetDogBreeds();
 const getDogPic = new GetDogPic();
 const submitBest = new SubmitBest();
-class App extends Component {
-  render() {
-    const welcome = "Welcome! Here are all the dogs!";
-    let likeDogs = null;
-    return (
-      <Grid>
-        <Row>
-          <Col md={2} />
-          <Col md={8}>
-            <div>
-              <HelloMessage welcome={welcome} likeDogs={likeDogs} />
-              <Favorites header="My favorite Dogs!">
-                <p>Akita</p>
-                <p>Husky</p>
-                <p>Pug</p>
-              </Favorites>
-              <p>What type of dog do you want to see!?</p>
-              <DogSelector getDogBreeds={getDogBreeds} getDogPic={getDogPic}/>
-              <SubmitBestDog submitBest={submitBest}/>
-            </div>
-          </Col>
-          <Col md={2} />
-        </Row>
-      </Grid>
-    );
-  }
+
+function App () {
+  const welcome = "Welcome! Here are all the dogs!";
+  let likeDogs = null;
+  return (
+    <Grid>
+      <Row>
+        <Col md={2} />
+        <Col md={8}>
+          <div>
+            <HelloMessage welcome={welcome} likeDogs={likeDogs} />
+            <Favorites header="My favorite Dogs!">
+              <p>Akita</p>
+              <p>Husky</p>
+              <p>Pug</p>
+            </Favorites>
+            <p>What type of dog do you want to see!?</p>
+            <DogSelector getDogBreeds={getDogBreeds} getDogPic={getDogPic}/>
+            <SubmitBestDog submitBest={submitBest}/>
+          </div>
+        </Col>
+        <Col md={2} />
+      </Row>
+    </Grid>
+  );
 }
 
 export default App;
